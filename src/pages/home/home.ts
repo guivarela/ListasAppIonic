4a13f123cad2edacdbffe9cd6b22e8d2629fb2bf
arqdsis <arqdsis@mocli10238.animaedu.intranet>
2018-05-16 20:47:45 -0300

    navegacao e listas

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,28 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { Livro } from '../../model/livro';
+import { DestinoPage } from '../destino/destino';
 
 @Component({
-  selector: 'page-home',
-  templateUrl: 'home.html'
+	selector: 'page-home',
+	templateUrl: 'home.html'
 })
 export class HomePage {
+	public livros: Livro[];
 
-  constructor(public navCtrl: NavController) {
-
-  }
-
+	constructor(public navCtrl: NavController) {	
+		var l1 = { nome: '1984', autor: 'George Orwell',img:'assets/imgs/books.jpg' };
+		var l2 = { nome: 'Harry Potter e a Pedra Filosofal', autor: 'J. K. Rowling',img:'assets/imgs/books.jpg' };
+		var l3 = { nome: 'O Senhor dos Anéis', autor: 'J. R. R. Tolkien',img:'assets/imgs/books.jpg' };
+		var l4 = { nome: 'Fundação', autor: 'Isaac Asimov',img:'assets/imgs/books.jpg' };
+		var l5 = { nome: 'Neuromancer', autor: 'William Gibson',img:'assets/imgs/books.jpg' };
+		var l6 = { nome: 'O Homem do Castelo do Alto', autor: 'Philip K. Dick',img:'assets/imgs/booke.jpg' };
+		var l7 = { nome: 'Snow Crash', autor: 'Neal Stephenson',img:'assets/imgs/booke.jpg' };
+		var l8 = { nome: '2001: Uma Odisséia no Espaço', autor: 'Arthur Clarke',img:'assets/imgs/booke.jpg' };
+		var l9 = { nome: 'Coraline', autor: 'Neal Gaiman',img:'assets/imgs/booke.jpg' };
+		this.livros = [l1, l2, l3, l4, l5, l6, l7, l8, l9];
+	}
+	irParaDestino(livro:Livro):void{
+		this.navCtrl.push(DestinoPage, {livroSelecionado: livro});
+		}
 }
